6f63932f801e01cdabed4fa1228034e0c23fe900
Joseph Miller <josephclaymiller@gmail.com>
2020-08-08 00:24:22 -0700

    create bpm calculator

--- BPM Calculator/ViewController.swift
+++ BPM Calculator/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var bpm = 0
+    var taps = 0
+    var lastTime: Date?
+    
+    @IBOutlet weak var bpmLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapScreen(_ sender: Any) {
+        taps += 1
+        bpm = Int(calculateBPM())
+        print(bpm)
+        bpmLabel.text = String(bpm)
+    }
+    
+    func timePassed() -> TimeInterval {
+        // calculate time passed
+        if lastTime == nil {
+            lastTime = Date()
+        }
+        return lastTime!.timeIntervalSinceNow * -1
+    }
+    
+    func calculateBPM() -> Double {
+        // taps in time passed
+        let timePassedSeconds = timePassed()
+        let timePassedMinutes = (timePassedSeconds/60)
+        if timePassedSeconds < 1 {
+            return 0
+        }
+        return Double(taps) / timePassedMinutes
+    }
+    
 }
 
